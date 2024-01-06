3e5d645510bb64e433d14982f9a4df0a08b49537
jeff122iq <jeff122iq@gmail.com>
2024-01-07 01:18:58 +0200

    add variiants of header and different layouts for pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 'use client';
 
-import Header from '@/components/Header';
-import ArrLeft from '@/public/arr-left';
-import Image from 'next/image';
-import avatar from '@/public/image/avatar.png';
 import TeaSearch from '@/components/TeaSearch';
 import TeaFeatured from '@/components/TeaFeatured';
-import NavFooter from '@/components/NavFooter';
+import HomeLayout from '@/app/HomeLayout';
 
 export default function Home() {
   return (
-    <main>
-      <Header>
-        <div className="flex justify-between">
-          <ArrLeft />
-          <Image src={avatar} width={40} height={40} alt="avatar" />
-        </div>
-      </Header>
+    <HomeLayout>
       <TeaSearch />
       <TeaFeatured />
-      <NavFooter />
-    </main>
+    </HomeLayout>
   );
 }
