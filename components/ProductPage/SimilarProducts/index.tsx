6bd5f4a0967c4be02e83be281a889181fae43c9a
NastyaSkreka <skreka.nastya@gmail.com>
2024-01-20 21:37:23 +0200

    refactor: similar product section - product page to styled component

--- components/ProductPage/SimilarProducts/index.tsx
+++ components/ProductPage/SimilarProducts/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import similar from '@/public/image/similarpr.png';
+import { 
+    SimilarProductsContainer,
+    Title,
+    ProductContainer,
+    ProductImage,
+    ProductDetails,
+    ProductName,
+    RatingContainer,
+    RatingValue,
+    Select
+} from './styles'
+import StarIcon from '@/public/start-icon'
 
 export default function SimilarProducts() {
   return (
-    <div>SimilarProducts</div>
+    <SimilarProductsContainer>
+    <Title>Similar products</Title>
+    <ProductContainer>
+      <ProductImage src={similar} alt="product" />
+      <ProductDetails>
+        <ProductName>pure Milk Tea</ProductName>
+        <RatingContainer>
+          <RatingValue>4.8</RatingValue>
+          <StarIcon />
+        </RatingContainer>
+      </ProductDetails>
+      <Select />
+    </ProductContainer>
+  </SimilarProductsContainer>
   )
 }
