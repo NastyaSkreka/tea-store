0ba7350949a85e318c6ae846cf48ade9823425dc
NastyaSkreka <skreka.nastya@gmail.com>
2024-01-20 21:31:03 +0200

    refactor: about product section - product page to styled component

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+'use client';
+
+import ProductLayout from '@/components/Layouts/ProductLayout';
+import AboutProduct from '@/components/ProductPage/AboutProduct';
+import SimilarProducts from '@/components/ProductPage/SimilarProducts';
 
 export default function Product() {
-  return <div>Product page</div>;
+    return (
+        <ProductLayout>
+          <AboutProduct/>
+          <SimilarProducts/>
+        </ProductLayout>
+      );
 }
+
---
+++ public/clock-icon.tsx
@@ -0,0 +1,48 @@
+export default function Clock() {
+    return (
+        <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_i_310_267)">
+        <circle cx="11.5" cy="11.5" r="10.5" stroke="#00575F" stroke-width="2"/>
+        </g>
+        <g filter="url(#filter1_i_310_267)">
+        <line x1="10.5081" y1="12.4284" x2="15.8155" y2="16.5564" stroke="#00575F" stroke-linecap="round"/>
+        </g>
+        <g filter="url(#filter2_i_310_267)">
+        <line x1="10.6313" y1="5.5684" x2="10.4814" y2="12.2905" stroke="#00575F" stroke-linecap="round"/>
+        </g>
+        <defs>
+        <filter id="filter0_i_310_267" x="0" y="0" width="23" height="27" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_310_267"/>
+        </filter>
+        <filter id="filter1_i_310_267" x="10.0081" y="11.9284" width="6.30746" height="9.12804" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_310_267"/>
+        </filter>
+        <filter id="filter2_i_310_267" x="9.98135" y="5.0684" width="1.14996" height="11.7221" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_310_267"/>
+        </filter>
+        </defs>
+        </svg>
+        
+    );
+  }
