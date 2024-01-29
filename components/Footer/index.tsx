413d466d11a080d85d38940415dc864bc60f5b64
NastyaSkreka <skreka.nastya@gmail.com>
2024-01-29 21:17:53 +0200

    feat: add footer active link

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,4 @@
 "use client"
-import React, { useEffect, useState } from 'react';
 
 import Home from '@/public/home-icon';
 import Heart from '@/public/heart-icon';
@@ -8,20 +7,11 @@ import User from '@/public/user-icon';
 import {
     FooterNav,
     IconsContainer,
-    NavItemActive,
-    NavItem, 
-    FooterContainer
-    
+    NavItem
 } from './styles'
-import Button from '../ui/Button';
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
-
-
-interface IProps  {
-  variant?: 'nav' | 'btn-full' | 'btn-border';
-}
 
 const navLinks = [
     {id: 1, icon: <Home/>, path: '/'}, 
@@ -31,45 +21,11 @@ const navLinks = [
 ]
 
 
-// const checkVariant = (variant?: 'nav' | 'btn-full' | 'btn-border') => {
-//     // const { pathname } = useRouter();
-//     const pathname = usePathname();
-
-//   switch (variant) {
-//     case 'nav':
-//       return  <FooterNav>
-//          <IconsContainer>
-//          {navigation.map(({ id, icon, path }) => (
-//                 <Link key={id} href={path} passHref>
-//                 <NavItem className={pathname === path ? 'active' : ''}>
-//                     {icon}
-//                 </NavItem>
-//                 </Link>
-//             ))}
-//     </IconsContainer>
-//     </FooterNav>
-
-//     case 'btn-full':
-//       return <Button color="green" label="Add to chart" />
-          
-//     case 'btn-border':
-//       return <Button color="white" label="Explore" />
-//     default:
-//       return null
-//   }
-// }
-
-const NavLinkItem = () => {
-
-}
-
-
-export default function Footer({variant}: IProps) {
-const pathname = usePathname();
+export default function Footer() {
+    const pathname = usePathname();
 
   return (
-      <FooterContainer>
-        <FooterNav>
+    <FooterNav>
     <IconsContainer>
       {navLinks.map((navLink) => (
         <Link key={navLink.id} href={navLink.path} passHref>
@@ -78,9 +34,8 @@ const pathname = usePathname();
             </NavItem>
         </Link>
       ))}
-
     </IconsContainer>
     </FooterNav>
-    </FooterContainer>
+ 
   );
 }
--- components/Footer/styles.ts
+++ components/Footer/styles.ts
@@ -23,36 +23,21 @@ export const IconsContainer = styled.div`
     align-items: center;
     gap: 30px;
 `
-export const NavItemActive  = styled.div`
-  border-radius: 10px;
-  padding: 5px;
-`;
 
 export const NavItem = styled.div`
   padding: 10px;
   cursor: pointer;
+  border-radius: 10px;
   transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
 
   svg {
     width: 20px;
     height: 20px;
   }
 
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-
   &.active {
-    background-color: #a0a0a0;
+    border-radius: 10px;
+    padding: 10px;
+    background-color: #00575F;
   }
 `;
-
-
-export const FooterContainer = styled.footer`
-    margin-top: 23px;
-`
